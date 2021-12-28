1a75abe1cf6ea08c4789bb683b744d3726c859e8
jardersilva <jardersilva@outlook.com>
2021-12-28 14:12:46 -0300

    yrtyrtyr

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,11 @@ const io = initIO(server);
 //app.use(io);
 
 app.use(cors());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(Sentry.Handlers.requestHandler());
