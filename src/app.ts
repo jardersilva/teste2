30debc7b382494abd02a481443ed25d0043f950d
jardersilva <jardersilva@outlook.com>
2021-12-28 13:54:34 -0300

    asdasdad

--- src/app.ts
+++ src/app.ts
@@ -15,24 +15,8 @@ import { logger } from "./utils/logger";
 Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 const app = express();
+const server = require('http').Server(app);
 app.use(cors());
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use(cookieParser());
 app.use(express.json());
@@ -52,4 +36,4 @@ app.use(async (err: AppError, req: Request, res: Response, _: NextFunction) => {
   return res.status(500).json({ error: "Internal server error" });
 });
 
-export default app;
+export default server;
